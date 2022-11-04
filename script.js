4690d2e1ff29d83688eb44cbd5401cd21c9d92c2
Dima2323-tech <65080866+Dima2323-tech@users.noreply.github.com>
2022-11-04 13:29:35 +0200

    Ver 2

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 //1) Створити функцію яка отримує невизначену кількість аргументів і вираховує їх суму +++
-let valuesС = parseInt(prompt('Ведите количестов задаваемых чисел'))
-let arrValue = [];
-function sum(arg1  , arg3){
+function sum(...arg){
+    console.log(arg)
 
-    for (i = 0; i < valuesС; i++){
-        let values = parseInt(prompt(`Введите ${valuesС} чисел`))
-        arg3.push(values)
-    }
-    console.log(arg3)
     let sum = 0;
-
-    for (let item of arg3){
+    for (let item of arg){
         sum += item
     }
     console.log(sum)
 }
-sum(valuesС  , arrValue)
-//2)Створити функцію яка отримує від користувача день народження,місяць, +++
-// рік і повертає рік народження і якщо сьогодні день народження користувача то вивести привітання
+sum( 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8)
+// //2)Створити функцію яка отримує від користувача день народження,місяць, +++
+// // рік і повертає рік народження і якщо сьогодні день народження користувача то вивести привітання
 let day = parseInt(prompt("Введите день рождения"))
 let month1 = parseInt(prompt("Введите месяц рождения"))
 let year = parseInt(prompt("Введите год рождения"))
@@ -35,7 +28,7 @@ function congrats(arg1 , arg2){
     }
 }
 console.log(congrats(bday , normalday))
-//3) Створити функцію яка з массиву вибирає всі булеві значення
+// //3) Створити функцію яка з массиву вибирає всі булеві значення
 
 const sort = [true, 'false' , 111, -0, 'true', false, {status:true}, [true , 'false']];
 
@@ -57,21 +50,21 @@ function getBoolean(argS){
 console.log(getBoolean(sort))
 
 
-// console.log(replacement())
-// 4) Створити функцію яка повертає сторіччя, функція отримує рік +++
-// 1810->19
-// 1700 -> 17
-// 1601 ->17
-// 2000 -> 20
-
+// // console.log(replacement())
+// // 4) Створити функцію яка повертає сторіччя, функція отримує рік +++
+// // 1810->19
+// // 1700 -> 17
+// // 1601 ->17
+// // 2000 -> 20
+//
 let year1 = parseInt(prompt("Введите год"))
 function getСentury(year){
-    let century = Math.floor((year - 10)/100) + 1
+    let century = Math.floor((year - 1)/100) + 1
     console.log(`Вы ввели ${year} год - Он равен ${century} веку`)
 }
 console.log(getСentury(year1))
 
-// 5) Створити функцію яка повертає скільки днів в цьому місяці а також в наступному  ( В цьому місяці 30 днів в наступному 31 )
+// // 5) Створити функцію яка повертає скільки днів в цьому місяці а також в наступному  ( В цьому місяці 30 днів в наступному 31 )
 let date = new Date();
 let month = date.getMonth()
 let year2 = date.getFullYear()
